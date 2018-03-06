b1ed90d37feb9891562a446405aff02f56ef1d57
ThePenguinDuck <marcopiest@gmail.com>
2018-03-06 10:49:46 +0100

    init

---
+++ bus-project/Models/GeoLocation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Device.Location;
+
+namespace bus_project.Model
+{
+    public class GeoLocation
+    {
+        public GeoLocation()
+        {
+            GeoCoordinateWatcher watcher = new GeoCoordinateWatcher();
+            watcher.PositionChanged += watcher_PositionChanged;
+            watcher.Start();
+        }
+
+        private void watcher_PositionChanged(object sender, GeoPositionChangedEventArgs<GeoCoordinate> e)
+        {
+            string.Format("Lat: {0}, Long: {1}", e.Position.Location.Latitude, e.Position.Location.Longitude);
+        }
+    }
+}
---
+++ bus-project/Models/WhatsappSender.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WhatsAppApi;
+
+namespace bus_project.Model
+{
+    public class WhatsappSender
+    {
+        public WhatsappSender()
+        {
+            string from = "hier mobiel nummer van bus-systeem";
+            string token = "hier whatsapp token van WART (https://github.com/mgp25/WART)";
+
+            WhatsApp wa = new WhatsApp(from, token, "Bus-systeem", false, false);
+
+            wa.OnConnectSuccess += () =>
+            {
+                //MessageBox.Show("Connecten naar whatsapp..."); // Alleen voor testen
+
+                wa.OnLoginSuccess += (phoneNumber, data) =>
+                {
+                    // wa.SendMessage(to, msg);
+                    //MessageBox.Show("Bericht verstuurd.."); // Alleen voor testen
+                };
+
+                wa.OnLoginFailed += (data) =>
+                {
+                    //MessageBox.Show("Login mislukt : {0}", data); // Alleen voor testen
+                };
+
+                wa.Login();
+
+            };
+
+            wa.OnConnectFailed += (ex) =>
+            {
+                //MessageBox.Show("Connection mislukt"); // Alleen voor testen
+            };
+
+            wa.Connect();
+        }
+    }
+}
